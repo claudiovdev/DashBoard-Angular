6f65d4349f5d88477271954527f988f2458ffe16
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2021-06-28 21:24:55 -0300

    Criando o dashboard.module, index.ts e importando para o module principal

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DashboardModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
